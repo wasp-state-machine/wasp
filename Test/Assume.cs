5ec31bec9aba93dbdde619ff9d4e33257fd17a03
q-0q <jrwthrs@gmail.com>
2024-10-21 20:48:54 -0400

    feat: Assume

---
+++ Test/Assume.cs
@@ -0,0 +1,38 @@
+﻿namespace Test;
+
+using Wasp;
+using Xunit;
+
+public class Assume
+{
+    [Fact]
+    public void Basic()
+    {
+        var machine = new Machine<State, Trigger>(State.A);
+
+        bool b1 = false;
+        bool b2 = true;
+        bool b3 = false;
+        
+        machine.Configure(State.A)
+            .Permit(Trigger.X, State.B)
+            .OnExit(_ => b1 = true)
+            .OnExit(_ => b2 = false);
+
+        machine.Configure(State.B)
+            .OnEntry(_ => b3 = true);
+        
+        machine.Assume(State.B);
+        
+        Assert.False(b1);
+        Assert.True(b2);
+        Assert.False(b3);
+        
+        machine.Assume(State.A);
+        machine.Fire(Trigger.X);
+        
+        Assert.True(b1);
+        Assert.False(b2);
+        Assert.True(b3);
+    }
+}
--- Wasp/src/Machine.cs
+++ Wasp/src/Machine.cs
@@ -37,6 +37,11 @@ public partial class Machine<TState, TTrigger> where TState : notnull where TTri
     {
         _onTransitioned = action;
     }
+
+    public void Assume(TState state)
+    {
+        _currentState = state;
+    }
     
     private IDictionary<TState, StateConfig> _stateConfigs;
     private TState _currentState;
